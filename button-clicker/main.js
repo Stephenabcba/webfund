6cfbc4a0128d8eeff95aa0402a17e00055026ad2
Stephen Lee <stephenabcba@gmail.com>
2021-12-17 13:38:19 -0800

    added button clicker js practice

---
+++ button-clicker/main.js
@@ -0,0 +1,12 @@
+function logIn(element) {
+    element.innerText = "Logout"
+}
+
+function hide(element) {
+    element.remove()
+}
+
+
+function likeAlert(word) {
+    alert(word + " was liked")
+}
